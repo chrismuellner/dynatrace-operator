316225d02e47bbd785821d27d801e5d8c049caf4
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-06-21 15:13:37 +0200

    Uses the image for version label on oneagent daemonset

--- src/controllers/dynakube/oneagent/daemonset/daemonset.go
+++ src/controllers/dynakube/oneagent/daemonset/daemonset.go
@@ -128,8 +128,12 @@ func (dsInfo *builderInfo) BuildDaemonSet() (*appsv1.DaemonSet, error) {
 	instance := dsInfo.instance
 	podSpec := dsInfo.podSpec()
 
+	versionLabelValue := instance.Status.OneAgent.Version
+	if instance.Image() != "" {
+		versionLabelValue = instance.Image()
+	}
 	appLabels := kubeobjects.NewAppLabels(kubeobjects.OneAgentComponentLabel, instance.Name,
-		dsInfo.deploymentType, instance.Status.OneAgent.Version)
+		dsInfo.deploymentType, versionLabelValue)
 	labels := kubeobjects.MergeLabels(
 		appLabels.BuildLabels(),
 		dsInfo.hostInjectSpec.Labels,
