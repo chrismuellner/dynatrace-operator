d92552d93bc2da610aa64e9eece1224c7f993020
Christoph Muellner <christoph.muellner@dynatrace.com>
2022-06-22 17:23:03 +0200

    Remove cache from 3rd party libraries

--- hack/build/ci/download-go-build-deps.sh
+++ hack/build/ci/download-go-build-deps.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # get licenses if no cache exists
-if ! [ -d ./third_party_licenses ]; then
-  go install github.com/google/go-licenses@v1.2.1 && go-licenses save ./... --save_path third_party_licenses --force
-fi
+go install github.com/google/go-licenses@v1.2.1 && go-licenses save ./... --save_path third_party_licenses --force
 
 # fetch dependencies
 go get -d ./...
